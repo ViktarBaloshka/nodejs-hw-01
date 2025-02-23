9c9932904cce58d4fc3add16bc067618d4284531
Viktar Baloshka <vboloshko@gmail.com>
2025-02-23 13:32:28 +0100

    task 6-8 done: added scripts removeLastContacts. removeAllContacts, countContacts. Fixed path  contacts, import module fs

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,18 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  try {
+    const readAllContacts = await readContacts();
+
+    const removed = readAllContacts.slice(
+      readAllContacts,
+      readAllContacts.length - 1,
+    );
+    await writeContacts(removed);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 removeLastContact();
